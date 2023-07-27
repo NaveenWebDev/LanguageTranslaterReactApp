7de027535e4998795d4606a36888dc6345ef1377
NaveenWebDev <ns222280@gmail.com>
2023-07-27 13:56:12 +0530

    select option completed with api call

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
+import axios from 'axios';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [option, setOption] = useState([])
   const [to, setTo] = useState("")
   const [from, setFrom] = useState("")
   const [input, setInput] = useState("")
   const [Output, setOutput] = useState("")
+
+  useEffect(()=>{
+    axios.get("https://libretranslate.de/languages", {headers:{"accept":"application/json"}})
+    .then(res=>{
+      console.log(res.data)
+      setOption(res.data)
+    })
+  })
+
+  // curl -X 'GET' 'https://libretranslate.de/languages' -H 'accept: application/json'
 
   return (
     <div className="App">
       <div>
       <h3>From :</h3>
         <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
+          {option.map(opt=> <option value={opt.code}>{opt.name}</option> )}
         </select>
       </div>
       <h3>To :</h3>
       <div>
         <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
+        {option.map(opt=> <option value={opt.code}>{opt.name}</option> )}
         </select>
       </div>
 
